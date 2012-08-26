a7c533e8be350345bc3f4454ca90944e9cdaa653
ravik122 <ravik1222@gmail.com>
2012-08-26 13:03:34 +0200

    fixed todo installation and few other fixes

--- start.sh
+++ start.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 echo "configuring shell"
 mv .bashrc $HOME/
-echo "installing to-do manager:"
-git clone git@github.com:asb/sh-todo.git $HOME/inf/sh-todo
-mv .sh-todo $HOME/
-echo "configuring fluxbox"
-mv init $HOME/.fluxbox/
-mv keys $HOME/.fluxbox/
-mv startup $HOME/.fluxbox/
+
+#my own PATH dir
 echo "making .local/bin"
 mkdir $HOME/.local/bin
+
+#my own inf file
+echo "making inf file"
+mkdir $HOME/inf
+
+echo "installing to-do manager:"
+instal_dir=$HOME/inf/sh-todo
+path_dir=$HOME/.local/bin
+
+git clone git@github.com:asb/sh-todo.git $instal_dir
+mv .sh-todo $HOME/
+mkdir $instal_dir/files
+mkdir $path_dir/sh-todo
+cp $instal_dir/todo $path_dir/sh-todo/
+cp $instal_dir/todone $path_dir/sh-todo/
+
+echo "configuring fluxbox"
+instal_dir=$HOME/.fluxbox
+
+mv init $instal_dir/
+mv keys $instal_dir/
+mv startup $instal_dir/
+
 echo "copying scripts:"
 echo " background randomizer, "
-mv random_welcoming_screen $HOME/.local/bin/
+path_dir=$HOME/.local/bin
+
+mv random_welcoming_screen $path_dir/
 echo " time popup, "
-mv time-popup $HOME/.local/bin/
+mv time-popup $path_dir/
 echo " swap ctrl and caps lock "
 mv .Xmodmap $HOME/
+echo "reminding script,"
+mv remind $path_dir/
