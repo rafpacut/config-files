6db4a3e8f2fa8ef0a813bf15497700e1bdd25d90
rafpacut <rafpacut@gmail.com>
2016-01-04 21:00:02 +0100

    Added collecting unix timestamps to a text file

---
+++ time_tracker.sh
@@ -0,0 +1,18 @@
+#add on boot up command
+#/etc/rc.local is a file containing commands executed on boot up
+dir="$(pwd)"
+path_to_db="$dir/db.txt"
+
+touch $path_to_db
+
+sed -i "/# By default this script does nothing/a date +%s >> $path_to_db" /etc/rc.local
+
+
+#add on shutdown command
+#/ect/rc0.d/ is a directory containing symlinks to executable files
+on_shutdown="date +%s >> $path_to_db"
+echo $on_shutdown > $dir/on_shutdown.sh
+
+chmod +x $dir/on_shutdown.sh
+ln -s $dir/on_shutdown.sh /etc/rc0.d/K99time_spent_tracker.sh 
+
